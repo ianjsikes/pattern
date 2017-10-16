48bce4bcd06301e668b334c5792e798624fdcabb
Ian J Sikes <ianjsikes@gmail.com>
2017-10-16 16:41:45 -0700

    The train has departed! :steam_locomotive::train::train::train:

---
+++ index.js
@@ -0,0 +1,16 @@
+var css = require('sheetify')
+var choo = require('choo')
+
+css('tachyons')
+
+var app = choo()
+if (process.env.NODE_ENV !== 'production') {
+  app.use(require('choo-devtools')())
+  app.use(require('choo-log')())
+}
+
+app.route('/', require('./views/main'))
+app.route('/*', require('./views/404'))
+
+if (!module.parent) app.mount('body')
+else module.exports = app
